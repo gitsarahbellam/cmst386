4c12813325eaf5bede99e99b3dacc1a51de22647
gitsarahbellam <amypond@me.com>
2026-09-13 19:24:05 -0700

    Add Project 3

---
+++ project3/project3.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navList = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+    navList.classList.toggle("show");
+});
